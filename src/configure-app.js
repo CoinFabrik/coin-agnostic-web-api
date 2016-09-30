50b7c6a231612d59fc5f3d35ea2c24ec0c946de8
Nicolas Boskovic <nicolas.boskovic@coinfabrik.com>
2016-09-30 15:55:43 -0300

    On error return json

--- src/configure-app.js
+++ src/configure-app.js
@@ -40,7 +40,7 @@ function configureApp(registerRoutes) {
 
   // development error handler
   // will print stacktrace
-  /*  if (app.get('env') === 'development') {
+  /* if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
       res.status(err.status || 500);
       res.render('error', {
@@ -54,7 +54,7 @@ function configureApp(registerRoutes) {
   // no stacktraces leaked to user
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
+    res.json({
       message: err.message,
       error: {}
     });
--- src/routes/api.js
+++ src/routes/api.js
@@ -73,7 +73,7 @@ function configureRoutes(coin) {
         res.status(400);
         res.json({
           subCode: 1,
-          message: 'Error getting tx params' + err
+          message: 'Error getting tx params: ' + err
         })
       } else {
         res.status(200);
@@ -85,19 +85,27 @@ function configureRoutes(coin) {
   router.put('/rawTransaction', (req, res, next) => {
     var tx = req.body.transaction;
     console.log('transaction: ' + tx);
-    coin.sendRawTx(tx, (err, txid) => {
-      if (err) {
-        res.status(400);
-        //TODO: Incorporate Ethererum Full Node codes and messages
-        res.json({
-          subCode: 1,
-          message: "Error sending transaction: " + err
-        })
-      } else {
-        res.status(200);
-        res.json({success: true, txid: txid});
-      }
+    if (tx === null || tx === '') {
+      res.status(400);
+      res.json({
+        subCode: 1,
+        message: "Cannot send null tx"
+      })
+    } else {
+      coin.sendRawTx(tx, (err, txid) => {
+        if (err) {
+          res.status(400);
+          //TODO: Incorporate Ethererum Full Node codes and messages
+          res.json({
+            subCode: 1,
+            message: "Error sending transaction: " + err
+          })
+        } else {
+          res.status(200);
+      res.json({success: true, txid: txid});
+    }
     })
+  }
   })
 
   return router;
